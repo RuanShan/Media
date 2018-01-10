fae26bfd0f8e8f5f244a3ea70c3b8b7eb0c435c5
qinghe <david@getstore.cn>
2018-01-10 14:55:57 +0800

    remove database.yml from rep

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,13 +27,14 @@ append :linked_files, "config/database.yml", "config/secrets.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :default_env, { path: "/alidata/server/mysql:$PATH" }
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
