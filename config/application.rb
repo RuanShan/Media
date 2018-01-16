ad4e3cdc43bcf48fa3351daab6541e069e335ad3
qinghe <david@getstore.cn>
2018-01-16 10:06:22 +0800

    restore lib

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Wp
     config.exceptions_app = self.routes
     # Custom directories with classes and modules you want to be autoloadable.
     #config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/api)
-    config.autoload_paths += %w(lib app/api app/models/payment_settings app/models/concerns app/controllers/concerns app/validators app/services).map { |path| "#{Rails.root}/#{path}" }
+    config.eager_load_paths += %w(lib app/api app/models/payment_settings app/models/concerns app/controllers/concerns app/validators app/services).map { |path| "#{Rails.root}/#{path}" }
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
